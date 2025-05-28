8e39b8ddd1233db55cbac58888892e8df9b6d1e9
AMBI-21 <abdoulmbi21@gmail.com>
2025-05-28 12:58:24 +0100

    verification

---
+++ check_file_exist.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read  "Entrez le nom du fichier :"
+#nom_fichier
+if [ -f "$nom_fichier" ];then
+	echo "Le ficher `$nom_fichier` existe."
+else
+	echo "Le fichier `$nom_fichier` n'existe pas."
+fi
+
+
